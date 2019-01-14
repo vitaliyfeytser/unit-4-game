cd5109d2ae5a03a9c1db5f2e8bae4973f4293d98
VitaliyFeytser <vitaliyfeytser@gmail.com>
2019-01-14 03:24:20 -0800

    logic is solid

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,23 +16,24 @@ $(document).ready(function(){
     // Generate random number between 19 and 120
 
     // Math.floor(Math.random() * (max - min + 1)) + min;
-    // function randNumFunc() {
-    //     randNum = Math.floor(Math.random() * 102) + 19;
-    //     console.log("randNum :" + randNum);
-    //     $( "#random-num" ).html(randNum);
-    // }
-    // randNumFunc();
+
+    function randNumFunc() {
+        randNum = Math.floor(Math.random() * 102) + 19;
+        console.log("randNum :" + randNum);
+        $( "#random-num" ).html(randNum);
+    }
+    randNumFunc();
 
     // Generate random numbers for crystals between 1 and 9 --- and make sure that they are unique to each crystal
     function crystalRandGen () {
 
         var uniqueRandoms = [];
-        var numRandoms = 4;
+        var numRandoms = 9;
 
         function makeUniqueRandom() {
             // refill the array if needed
             if (!uniqueRandoms.length) {
-                for (var i = 1; i <= numRandoms; i++) {
+                for (var i = 1; i < numRandoms; i++) {
                     uniqueRandoms.push(i);
                 }
             }
@@ -49,24 +50,22 @@ $(document).ready(function(){
 
         for (var i = 0; i < 4; i++) {
             var rand = makeUniqueRandom();
-            if (i % numRandoms == 0) {
-                return;
-            }
+            console.log("rand: " + rand);
+        }
             
+            console.log("uniqueRandoms: " + uniqueRandoms);
+
+
             crystal1 = uniqueRandoms[0];
             crystal2 = uniqueRandoms[1];
             crystal3 = uniqueRandoms[2];
             crystal4 = uniqueRandoms[3];
 
-            // crystal1 = Math.floor(Math.random() * 9) + 1;
-            // crystal2 = Math.floor(Math.random() * 9) + 1;
-            // crystal3 = Math.floor(Math.random() * 9) + 1;
-            // crystal4 = Math.floor(Math.random() * 9) + 1;
             console.log("crystal1 :" + crystal1);
             console.log("crystal2 :" + crystal2);
             console.log("crystal3 :" + crystal3);
             console.log("crystal4 :" + crystal4);
-        // }
+
     };
     crystalRandGen();
 
@@ -141,21 +140,13 @@ $(document).ready(function(){
             }, 500);
         }
 
-        //         //  Start on click.
-        // $("#start").on("click", function() {
-        //     //  Set the button alert's timeout to run three seconds after the function's called.
-        //     delayButtonAlert = setTimeout(function() {
-        //     alert("Alert #2: Called 3 seconds after the start button is clicked.");
-        //     }, 3000);
-        // });
-
         if (crystalSum === randNum) {
             delayReset = setTimeout(function() {
                 userWins = userWins + 1;
                 console.log("userWins :" + userWins);
                 $("#announcement").html("You won!");
                 alert("You won!");
-                delayReset = setTimeout(function() {
+                delayReset = setTimeout(function() { // Delay erasure of total score at victory or defeat for 1 sec.
                     crystalSum = 0;
                     $( "#crystal-sum" ).html("Your current total is: " + crystalSum);
                 }, 1000);
